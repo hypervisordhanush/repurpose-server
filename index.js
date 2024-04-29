b9d3ac71b78e6669d42b79914ecd66f5c9668323
hypervisordhanush <115521880+hypervisordhanush@users.noreply.github.com>
2024-04-29 09:43:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
@@ -33,10 +33,6 @@ const allowCors = fn => async (req, res) => {
   return await fn(req, res)
 }
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
 
 module.exports = allowCors(handler)
 // using body parser
